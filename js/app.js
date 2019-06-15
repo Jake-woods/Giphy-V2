dd88eabac91af2a47914570f14321851fde6f70c
Jake-woods <jake.13.5.2016@gmail.com>
2019-06-15 15:09:41 +0100

    Basic design commit

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
-const giphy = {
-   api: 'https://api.giphy.com/v1/gifs/search?',
-   apiKey: '&api_key=KvlI7qrbEwAVr36bcLHvtPTSMZItjtnu',
-   search: `&q=${inputSearch}`
-}
+// const giphy = {
+//    api: 'https://api.giphy.com/v1/gifs/search?',
+//    apiKey: '&api_key=t1WyOpFfske7f7bnMBsZwgCNB7iVJvJi',
+//    search: `&q=${inputSearch}`
+// }
