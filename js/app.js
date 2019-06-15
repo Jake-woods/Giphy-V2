ed7c549801d078e5194e1be541da402984b81bc2
Jake-woods <jake.13.5.2016@gmail.com>
2019-06-15 15:54:37 +0100

    Slight changes commit

--- js/app.js
+++ js/app.js
@@ -3,3 +3,11 @@
 //    apiKey: '&api_key=t1WyOpFfske7f7bnMBsZwgCNB7iVJvJi',
 //    search: `&q=${inputSearch}`
 // }
+
+const returnedImgEle = document.querySelector(".page__image");
+const form = document.querySelector(".page__form");
+
+form.addEventListener("submit", e => {
+  e.preventDefault();
+  form.reset();
+});
