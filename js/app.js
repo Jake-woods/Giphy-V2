7c3309976804a6fee70bc28f7beeae3e76c3039c
Jake-woods <jake.13.5.2016@gmail.com>
2019-06-24 11:42:53 +0100

    Gif history functionality and styling

--- js/app.js
+++ js/app.js
@@ -1,6 +1,24 @@
 // UI selectors
 const returnedImgEle = document.querySelector(".page__image");
 const form = document.querySelector(".page__form");
+const historyUl = document.querySelector(".gif-history__list");
+const historyEle = document.querySelector(".gif-history");
+const historyEleClose = document.querySelector(".gif-history__close");
+const historyEleOpen = document.querySelector(".gif-history__open");
+
+// Helper
+const createImgEle = src => {
+  const li = document.createElement("li");
+  const img = document.createElement("img");
+
+  li.classList.add("gif-history__item");
+  img.classList.add("gif-history__image");
+
+  img.setAttribute("src", src);
+
+  li.appendChild(img);
+  historyUl.appendChild(li);
+};
 
 // Functionality
 form.addEventListener("submit", e => {
@@ -21,8 +39,19 @@ form.addEventListener("submit", e => {
         "src",
         data.data[randomIndex].images.original.url
       );
+
+      createImgEle(data.data[randomIndex].images.original.url);
     });
 
   e.preventDefault();
   form.reset();
+});
+
+// Toggle gif history visibility
+historyEleOpen.addEventListener("click", () => {
+  historyEle.classList.add("gif-history--visible");
+});
+
+historyEleClose.addEventListener("click", () => {
+  historyEle.classList.remove("gif-history--visible");
 });
