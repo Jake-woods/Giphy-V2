3a6b7970a5b97be03a1ee8af605c2dbff1f3e786
Jake-woods <jake.13.5.2016@gmail.com>
2019-06-24 10:01:00 +0100

    Main functionality added to change the source of image when input

--- js/app.js
+++ js/app.js
@@ -1,13 +1,28 @@
-// const giphy = {
-//    api: 'https://api.giphy.com/v1/gifs/search?',
-//    apiKey: '&api_key=t1WyOpFfske7f7bnMBsZwgCNB7iVJvJi',
-//    search: `&q=${inputSearch}`
-// }
-
+// UI selectors
 const returnedImgEle = document.querySelector(".page__image");
 const form = document.querySelector(".page__form");
 
+// Functionality
 form.addEventListener("submit", e => {
+  const formInput = document.querySelector(".page__form-query").value;
+  const giphySetup = {
+    api: "https://api.giphy.com/v1/gifs/search?",
+    apiKey: "&api_key=t1WyOpFfske7f7bnMBsZwgCNB7iVJvJi",
+    search: `&q=${formInput}`
+  };
+  const giphyUrl = giphySetup.api + giphySetup.apiKey + giphySetup.search;
+
+  fetch(giphyUrl)
+    .then(resp => resp.json())
+    .then(data => {
+      const randomIndex = Math.floor(Math.random() * data.data.length);
+
+      returnedImgEle.setAttribute(
+        "src",
+        data.data[randomIndex].images.original.url
+      );
+    });
+
   e.preventDefault();
   form.reset();
 });
